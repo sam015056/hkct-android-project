04cf33974bf7e577725d94ad447d5e617ca38659
sam015056 <s2081951@student.hkct.edu.hk>
2023-04-06 01:13:02 +0800

    .
    .

---
+++ app/src/main/java/com/hkct/project/informationActivity.java
@@ -0,0 +1,14 @@
+package com.hkct.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class informationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
