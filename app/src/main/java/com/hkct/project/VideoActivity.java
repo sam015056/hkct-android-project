14aff37162629e82a942baba2af3658b740326f0
sam015056 <117882694+sam015056@users.noreply.github.com>
2023-04-20 16:30:47 +0800

    sam
    video

--- app/src/main/java/com/hkct/project/VideoActivity.java
+++ app/src/main/java/com/hkct/project/VideoActivity.java
@@ -2,7 +2,10 @@ package com.hkct.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.badminton_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
     }
 }
